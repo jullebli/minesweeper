327bd18ca5fa8c835336523ceb023011700ee614
Julia Bergman <julia.bergman@helsinki.fi>
2020-11-24 00:04:08 +0200

    Lisää harjoitustyön maven projekti ja muokkaa .gitignore

---
+++ Minesweeper/src/main/java/minesweeper/Game.java
@@ -0,0 +1,17 @@
+
+package minesweeper;
+
+
+public class Game {
+    private int width;
+    private int height;
+    
+    public Game(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
