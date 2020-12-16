609ab7bdb5e359b5942f9fafef0ea08aaaab9539
Julia Bergman <julia.bergman@helsinki.fi>
2020-12-16 21:50:51 +0200

    Game Konstruktorimuutoksia

--- Minesweeper/src/main/java/minesweeper/domain/Game.java
+++ Minesweeper/src/main/java/minesweeper/domain/Game.java
@@ -20,9 +20,14 @@ public class Game {
     final String saveGameMagic = "Minesweepersavegame";
     final int saveGameVersion = 1;
 
-    private Game(int width, int height, boolean randomMines) {
+    private Game(int width, int height, String[] mineMap) {
+        if (mineMap != null) {
+            width = mineMap.length;
+            height = mineMap[0].length();
+        }
         this.width = width;
         this.height = height;
+
         this.running = true;
         this.victory = false;
         this.open = new boolean[height][width];
@@ -31,7 +36,22 @@ public class Game {
 
         this.openedSquares = 0;
 
-        if (randomMines) {
+        if (mineMap != null) {
+            totalMines = 0;
+
+            for (int y = 0; y < getHeight(); y++) {
+                for (int x = 0; x < getWidth(); x++) {
+                    if (mineMap[y].charAt(x) == 'M') {
+                        totalMines++;
+                        mine[y][x] = true;
+                    }
+                    if (mineMap[y].charAt(x) == 'F') {
+                        flag[y][x] = true;
+                    }
+                }
+            }
+
+        } else {
             this.totalMines = 10;
             placeRandomMines(totalMines);
 
@@ -40,27 +60,13 @@ public class Game {
 
     public Game(int width, int height) {
         //create Game with randomized mine locations
-        this(width, height, true);
+        this(width, height, null);
     }
 
     public Game(String[] mineMap) {
         //create Game with String array given mine locations
         //for test purposes only!
-        this(mineMap.length, mineMap[0].length(), false);
-
-        totalMines = 0;
-
-        for (int y = 0; y < getHeight(); y++) {
-            for (int x = 0; x < getWidth(); x++) {
-                if (mineMap[y].charAt(x) == 'M') {
-                    totalMines++;
-                    mine[y][x] = true;
-                }
-                if (mineMap[y].charAt(x) == 'F') {
-                    flag[y][x] = true;
-                }
-            }
-        }
+        this(0, 0, mineMap);
 
     }
 
@@ -74,7 +80,7 @@ public class Game {
 
     /**
      * Returns if the game is still running.
-     * 
+     *
      * @return true if game is running, false otherwise
      */
     public boolean isRunning() {
@@ -88,14 +94,14 @@ public class Game {
     public boolean isOpen(int x, int y) {
         return open[y][x];
     }
-    
+
     public boolean isFlag(int x, int y) {
         return flag[y][x];
     }
 
     /**
      * Places mines randomly on the minefield.
-     * 
+     *
      * @param totalMines number of mines to place
      */
     private void placeRandomMines(int totalMines) {
@@ -115,10 +121,10 @@ public class Game {
 
     /**
      * Opens a square on the minefield
-     * 
+     *
      * @param x x coordinate of the square
      * @param y y coordinate of the square
-     * 
+     *
      * @return true if game is running, false otherwise
      */
     public boolean open(int x, int y) {
@@ -144,12 +150,12 @@ public class Game {
 
         return running;
     }
-    
+
     private void openNeighbouringSquares(int x, int y) {
         if (countMines(x, y) == 0) {
             // if 0 mines, go through neigbouring squares
             int[][] neighbours = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1},
-                                  {1, 0}, {1, -1}, {0, -1}};
+            {1, 0}, {1, -1}, {0, -1}};
 
             for (int i = 0; i < 8; i++) {
                 int nY = y + neighbours[i][0];
@@ -163,17 +169,17 @@ public class Game {
 
     /**
      * Counts the mines in the neighbouring squares of a square.
-     * 
+     *
      * @param x x coordinate of a square
      * @param y y coordinate of a square
-     * 
+     *
      * @return number of mines in the neighbouring squares
      */
     public int countMines(int x, int y) {
         //count mines in neighbouring squares
         int mineCount = 0;
         int[][] neighbours = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1},
-                              {1, 0}, {1, -1}, {0, -1}};
+        {1, 0}, {1, -1}, {0, -1}};
 
         for (int i = 0; i < 8; i++) {
             int nY = y + neighbours[i][0];
@@ -184,10 +190,10 @@ public class Game {
         }
         return mineCount;
     }
-    
+
     /**
      * Sets a flag in one square.
-     * 
+     *
      * @param x x coordinate of a square
      * @param y y coordinate of a square
      * @param placed true when setting a flag, false when removing a flag
@@ -196,26 +202,25 @@ public class Game {
         //sets a flag in one square, placed=true when setting a flag
         //false when removing a flag
         //todo? check running? open?
-        flag[y][x] = placed;      
+        flag[y][x] = placed;
     }
 
     /**
-     * Checks if the square is on the game board, within height and width of
-     * the board
-     * 
+     * Checks if the square is on the game board, within height and width of the
+     * board
+     *
      * @param x x coordinate of a square
      * @param y y coordinate of a square
-     * 
+     *
      * @return true if square is on board, false if square is not on board
      */
     private boolean isOnBoard(int x, int y) {
         return !(x < 0 || y < 0 || x >= width || y >= height);
     }
-    
+
     /**
-     * Forms a StringBuilder type variable from a 2-dimensional boolean
-     * array.
-     * 
+     * Forms a StringBuilder type variable from a 2-dimensional boolean array.
+     *
      * @param array 2-dimensional boolean array
      * @param s StringBuilder type variable that will be appended
      */
@@ -229,33 +234,32 @@ public class Game {
         s.append("\n");
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
-                s.append(array[y][x] ? "1" : "0");            
+                s.append(array[y][x] ? "1" : "0");
             }
             s.append("\n");
         }
     }
-    
+
     /**
      * Saves the game by writing it to a file where it can be read from later.
-     * 
+     *
      * @param filename name of the file where the saved game will be written
-     * 
+     *
      * @throws IOException throws exception if writing to the file fails
      */
     public void saveGameToFile(String filename) throws IOException {
         FileWriter writer = new FileWriter(filename);
         BufferedWriter bw = new BufferedWriter(writer);
-        
+
         bw.write(toString());
         bw.close();
     }
 
-    
     @Override
     public String toString() {
         //used for saving the game
         StringBuilder s = new StringBuilder();
-        
+
         s.append(saveGameMagic);
         s.append("\n");
         s.append(saveGameVersion);
@@ -263,7 +267,7 @@ public class Game {
         arrayToStringBuilder(mine, s);
         arrayToStringBuilder(open, s);
         arrayToStringBuilder(flag, s);
-       
+
         return s.toString();
     }
 }
